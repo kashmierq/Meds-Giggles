5e44932accfe71e0f2d16540b8f8db70359f2240
kashmierq <kashmierquinones@gmail.com>
2025-11-20 22:01:14 +0800

    PHP

---
+++ register_user.php
@@ -0,0 +1,16 @@
+<?php
+include 'connect.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $username, $email, $password);
+
+    if ($stmt->execute()) {
+        echo "Registration successful!" <a href='login.php'>Login here</a>;
+    } else {
+        echo "Error: " . $stmt->error;
+    }
